6b8a6e77276f901c8217629e6a1a5c76f1a37b90
Shubham Yadav <146001625+royshubham305@users.noreply.github.com>
2025-07-13 22:33:08 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
